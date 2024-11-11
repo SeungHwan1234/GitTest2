fed673033d9b2c89617f262d4b75766ac24292d5
chash <chash0941@gmail.com>
2024-11-11 23:54:54 +0900

    ViewBinding Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.lion.gittest2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.lion.gittest2"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    viewBinding {
+        enable = true
     }
 }
 
